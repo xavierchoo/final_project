a1f5e20fe80269db13a417780bd2781db79395f7
XAvier Choo <xavierzchoo@hotmail.com>
2018-04-24 10:42:35 +0800

    Created User setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < Clearance::UsersController
+	def create
+		
+		@user =User.new(register_params)
+		if @user.save
+		    sign_in @user
+		    redirect_to '/'
+		else
+		    render template: "users/new"
+		end
+	end
+
+	private
+
+	def register_params
+		params.require(:user).permit(:first_name , :last_name , :username ,:email ,:password)
+	end
+
+end
