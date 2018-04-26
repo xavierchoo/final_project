77ca0d6d356adb8672e0446b3989dc5bbc10fa80
Edwin Capel <edwincapel@Edwins-MacBook-Pro.local>
2018-04-26 18:14:06 +0800

    moved index page & added logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,8 @@ class WelcomeController < ApplicationController
 	# 	n = News.new("e9a7bf1016d247af9980045693f8f46b")
 	# 	@index = n.get_top_headlines(sources: "bbc-news")
 	# end
+  def index
+  	@articles = Testing.all
+  end
 
 end
