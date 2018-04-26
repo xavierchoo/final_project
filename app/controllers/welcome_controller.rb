2c4b9a51767ba1eecbe6ba1ed54ab7cdc575398f
lwsiew <liangwei_siew@hotmail.com>
2018-04-26 11:33:53 +0800

    26/4/18

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
 
-	def index
-		n = News.new("e9a7bf1016d247af9980045693f8f46b")
-		@index = n.get_top_headlines(sources: "bbc-news")
-	end
+	# def index
+	# 	n = News.new("e9a7bf1016d247af9980045693f8f46b")
+	# 	@index = n.get_top_headlines(sources: "bbc-news")
+	# end
 
 end
