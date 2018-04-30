6905f83e8a347667d466cf8ebb66dc7cddf0410a
lwsiew <liangwei_siew@hotmail.com>
2018-04-30 17:21:23 +0800

    viewdone

--- db/migrate/20180430074632_add_views_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddViewsToArticles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :articles, :views, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_30_074632) do
+ActiveRecord::Schema.define(version: 2018_04_30_082758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2018_04_30_074632) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
-    t.integer "views"
+    t.integer "views", default: 0
   end
 
   create_table "bbcs", force: :cascade do |t|
