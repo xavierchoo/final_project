3a4b40dd85e414fa19bb6182b0774ddc93c3cff6
lwsiew <liangwei_siew@hotmail.com>
2018-05-03 13:23:04 +0800

    afternoon

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,16 +1,13 @@
 class FollowsController < ApplicationController
 	
 	def create
-<<<<<<< HEAD
 
-=======
->>>>>>> 44327605c15ddf17e8f4d5a6f990a557e21cc46e
 		@user = User.find(params[:follower_id])
 		if Follow.where(follower_id: current_user.id,user_id: @user.id).exists?
 		else
 			follow = Follow.new(follower_id: current_user.id, user_id: @user.id)
 			if follow.save
-				redirect_to admin_index_path
+				redirect_to user_path(@user.id)
 			else
 				redirect_to "/"
 			end
